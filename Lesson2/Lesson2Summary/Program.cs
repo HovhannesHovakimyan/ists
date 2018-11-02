e70a1d16242b4f58b50dc4850c2286a81be96380
Hovhannes Hovakimyan <SirusVirus@live.com>
2018-11-02 14:45:06 +0400

    task32

--- Lesson2/Lesson2Summary/Program.cs
+++ Lesson2/Lesson2Summary/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson2Summary
+namespace Task32
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter N natural number:");
+            int n = Convert.ToInt32(Console.Read());
+
+            int i;
+            float n1 = 1, result = 1;
+
+            for (i = 1; i < n; i++)
+            {
+                n1 *= i;
+                result += 1 / n1;
+            }
+
+            Console.WriteLine($"result: {result}");
+            Console.ReadKey();
         }
     }
 }
