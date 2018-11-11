8dd5c62e94571ed17d428bf57b789c38d5c59b98
Hovhannes Hovakimyan <SirusVirus@live.com>
2018-11-12 00:38:31 +0400

    go

--- OOP_this_base/This_and_Base/Program.cs
+++ OOP_this_base/This_and_Base/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace This_and_Base
 {
+    class Book
+    {
+        public int ID { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Book book = new Book();
+            book.ID = 1;
+
+            ChangeID(book);
+
+            Console.WriteLine(book.ID);
+        }
+
+        static void ChangeID(Book book)
+        {
+            book.ID = -1;
         }
     }
 }
