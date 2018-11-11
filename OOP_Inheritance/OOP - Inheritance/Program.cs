73792ebc63d218f3a376e05e19cdf26b6f34b85e
Hovhannes Hovakimyan <SirusVirus@live.com>
2018-11-11 17:56:21 +0400

    working

--- OOP_Inheritance/OOP - Inheritance/Program.cs
+++ OOP_Inheritance/OOP - Inheritance/Program.cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace OOP___Inheritance
 {
+    class Human
+    {
+        public string name { get; set; }
+        public string sex { get; set; }
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public void sayHello()
+        {
+            Console.WriteLine("Hello people!");
+        }
+    }
+
+    class Alkash : Human
+    {
+        public int gradus { get; set; }
+
+        public void sayBarlusner()
+        {
+            Console.WriteLine("Bayylusner boooloriin");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Alkash mard1 = new Alkash
+            {
+                name = "Valodik",
+                sex = "male"
+            };
+
+            Console.WriteLine(mard1.name);
+            Console.WriteLine(mard1.sex);
+
+            mard1.sayBarlusner();
+
+            Console.ReadKey();
         }
     }
 }
