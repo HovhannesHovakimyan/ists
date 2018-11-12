10855ae415a2e9f5306244bbd904301c07e02585
Hovhannes Hovakimyan <SirusVirus@live.com>
2018-11-12 17:39:02 +0400

    static added

---
+++ OOP_Static_NonStatic__Methods/Static_NonStatic__Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_NonStatic__Methods
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string argFirstName, string argLastName)
+        {
+            FirstName = argFirstName;
+            LastName = argFirstName;
+        }
+
+        public static string FormatFullName(string argFirstname, string argLastName)
+        {
+            return $"{argFirstname} - {argLastName}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Testik", "Testikyan");
+
+            Console.ReadKey();
+        }
+    }
+}
