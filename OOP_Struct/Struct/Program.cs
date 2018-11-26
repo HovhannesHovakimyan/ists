245877435bf162351e649a557c91a9682451799d
Hovhannes Hovakimyan <SirusVirus@live.com>
2018-11-26 20:05:46 +0400

    delegate added

--- OOP_Struct/Struct/Program.cs
+++ OOP_Struct/Struct/Program.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace Struct
 {
+    struct MyStruct
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
 
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"my name is {Name} and I'm {Age} years old.\n");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            MyStruct person = new MyStruct();
+            person.Name = "Valod";
+            person.Age = 18;
+
+            person.DisplayInfo();
+
+            //Delay
+            Console.ReadKey();
         }
     }
 }
