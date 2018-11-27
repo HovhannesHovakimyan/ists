8891b4459525d774e69e5eca66b603b2ce59e239
Hovhannes Hovakimyan <SirusVirus@live.com>
2018-11-27 14:25:47 +0400

    go

--- OOP_Delegate/Delegate/Program.cs
+++ OOP_Delegate/Delegate/Program.cs
@@ -8,8 +8,25 @@ namespace Delegate
 {
     class Program
     {
+        delegate void Message();
+        delegate int Operation(int x, int y);
+
+        static void Display() //compatible with Message() delegate
+        {
+            Console.WriteLine("Hello");
+        }
+
         static void Main(string[] args)
         {
+            Message message; //delegate determines a type, so variable can be that type
+            message = Display; //give a method which variable will point to
+
+            //massage = new Message(Display); //another way to pass method by creating object and passing method to it's constructor
+
+            //message.Invoke();  //another way to call delegate
+            message();
+
+            Console.ReadKey();
         }
     }
 }
